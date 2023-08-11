25d69fcb96af7219449f1ae0ee1e8e97906a92e6
Sndr00 <141956558+Sndr00@users.noreply.github.com>
2023-08-11 13:59:10 -0600

    Update Generaciones de computadoras.cpp

--- Generaciones de computadoras.cpp
+++ Generaciones de computadoras.cpp
@@ -1,11 +1,14 @@
-
-#include <stdio.h>
+#include <iostream>
 
 int main (int argc,char **argv)
+
 {
 
+	printf("Las generaciones de computadoras son categorías históricas utilizadas para clasificar el desarrollo y el avance de la tecnología en el campo de la informática. Comúnmente se clasifican en cinco generaciones, aunque algunos expertos pueden haber agregado generaciones adicionales o cambiado las clasificaciones.\n");
 	printf("\n");
+	printf("Primera generación (1946-1955)\n");
 	printf("\n");
-	printf("\n");
+	
+	
 	return 0;
 }
