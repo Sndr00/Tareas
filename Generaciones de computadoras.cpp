82ec79b8e57d6245b07ed11768c35febbe35abf7
Sndr00 <141956558+Sndr00@users.noreply.github.com>
2023-08-14 20:33:43 -0600

    Update Generaciones de computadoras.cpp

--- Generaciones de computadoras.cpp
+++ Generaciones de computadoras.cpp
@@ -6,24 +6,56 @@ int main (int argc,char **argv)
 
 {
 
-	printf("Las generaciones de computadoras son categorías históricas utilizadas para clasificar el desarrollo y el avance de la tecnología en el campo de la informática. Comúnmente se clasifican en cinco generaciones, aunque algunos expertos pueden haber agregado generaciones adicionales o cambiado las clasificaciones.\n");
-	printf("\n");
+printf("Las generaciones de computadoras son categorías históricas utilizadas para clasificar el desarrollo y el avance de la tecnología en el campo de la informática. Comúnmente se clasifican en cinco generaciones, aunque algunos expertos pueden haber agregado generaciones adicionales o cambiado las clasificaciones.\n");
+printf("\n");
+printf("\n");
 
-	printf("Primera generación (1946-1955)\n");
-	printf("Se desarrollaron las primeras computadoras, estas se caracterizaban por lo siguiente:\n");
+printf("Primera generación (1946-1955)\n");
+printf("Se desarrollaron las primeras computadoras, estas se caracterizaban por lo siguiente:\n");
+printf("\n");
+printf("Válvulas de vacío: Se las usaban para realizar operaciones lógicas y de procesamiento de datos.\n");
+printf("Tamaño y consumo de energía: Ocupaban habitaciones enteras y requerían un enfriamiento eficiente.\n");
+printf("Programación manual: Se conectaban y configurando interruptores y conexiones.\n");
+printf("Velocidad y capacidad limitadas: Eran muy lentas.\n");
+printf("\n");
+printf("\n");
+	printf("Segunda generación (1956-1963)\n");
+	printf("Las máquinas  se volvieron más eficientes, más pequeñas y más confiables. Las características de esta generación:\n");
 	printf("\n");
-	printf("Válvulas de vacío: Se las usaban para realizar operaciones lógicas y de procesamiento de datos.\n");
-	printf("Tamaño y consumo de energía: Ocupaban habitaciones enteras y requerían un enfriamiento eficiente.\n");
-	printf("Programación manual: Se conectaban y configurando interruptores y conexiones.\n");
-	printf("Velocidad y capacidad limitadas: Eran muy lentas.\n");
-	printf("Aplicaciones: Utilizaron principalmente en aplicaciones científicas y militares\n");
+	printf("Transistores: Estos eran mucho mas pequeños, usaban menos energía, generaban menos calor y eran más confiables.\n");
+	printf("Tamaño reducido: Eran mucho más pequeñas que las computadoras de primera generación.\n");
+	printf("Lenguajes de programación: Surgieron lenguajes de programación de alto nivel, como COBOL y FORTRAN.\n");
+	printf("Memorias magnéticas: Las cintas magnéticas y discos duros permitían un acceso más rápido y una mayor capacidad de almacenamiento.\n");
 	printf("\n");
-	
-		printf("Segunda generación (1956-1963)\n");
-		printf("Las máquinas  se volvieron más eficientes, más pequeñas y más confiables. Las características de esta generación:\n");
-		printf("\n");
-		printf("\n");
-		
-	
+	printf("\n");
+printf("Tercera Generación (1964-1971)\n");
+printf("Mejoro el rendimiento, la capacidad y la versatilidad de las computadoras. Las características principales son:\n");
+printf("\n");
+printf("Circuitos Integrados (Chips): Permitieron combinar múltiples componentes electrónicos en un solo chip \n");
+printf("Minicomputadoras: Estas máquinas eran menos costosas y más pequeñas.\n");
+printf("Sistemas Operativos Avanzados: Facilitó la programación en lenguajes de alto nivel.\n");
+printf("Lenguajes de Programación Avanzados: Hubo mejoras en la facilidad de uso y la eficiencia.\n");
+printf("Almacenamiento en Disco: Permitió un acceso más rápido a los datos y una mayor capacidad de almacenamiento.\n");
+printf("\n");
+printf("\n");
+printf("Cuarta Generación (1971-1980)\n");
+printf("Las computadoras ahora eran accesibles para el público y aumeQuinta Generación (1980-presente)nto la integración y accesibilidad en la informática. Se caracteriza por:\n");
+printf("\n");
+printf("Microprocesadores: Son chips que contienen una unidad central de procesamiento (CPU) completa en un solo circuito integrado. \n");
+printf("Computadoras Personales (PCs): Ahora había computadoras en los hogares y las pequeñas empresas.\n");
+printf("Memoria RAM Mejorada: Permitió un acceso más rápido a los datos y una mejora en el rendimiento general del sistema.\n");
+printf("Interfaces Gráficas de Usuario (GUI): Hicieron que la interacción con las computadoras fuera más intuitiva y accesible para un público más amplio.\n");
+printf("Redes de Computadoras: Permitió la comunicación y el intercambio de información entre diferentes sistemas.\n");
+printf("\n");
+printf("\n");
+    printf("Quinta Generación (1980-presente)\n");
+    printf("Se centra en aspectos avanzados y en desarrollos disruptivo. Los principales son:\n");
+    printf("\n");
+    printf("Inteligencia Artificial (IA): Permitiendo a las máquinas realizar tareas que anteriormente requerían la inteligencia humana.\n");
+    printf("Computación Cuántica: Utiliza los principios de la mecánica cuántica para realizar cálculos mucho más rápido. \n");
+    printf("Conectividad y Nube: Esto permite el acceso a recursos y datos desde cualquier lugar y fomenta la colaboración en línea.\n");
+    printf("Dispositivos Móviles y Tecnología Portátil: Los avances en la potencia de procesamiento, la duración de la batería y la miniaturización provoco la creación de tecnología portátil.\n");
+    printf("Interfaz Natural: Las interfaces de usuario son más naturales y accesibles, como comandos de voz, gestos y realidad aumentada.\n");
+    
 	return 0;
 }
